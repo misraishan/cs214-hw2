855336a30232ddd5f9878d8ec2ec42d22d92743d
HayHay <dev@hayhay.dev>
2022-10-11 12:18:24 -0400

    rework of dir using dirent

--- ls.c
+++ ls.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[]) {
-    FILE *dir = popen("ls", "r");
+    DIR *dir;
+    struct dirent *dirp;
+    char *path = ".";
 
-    if (dir == NULL) {
-        printf("Error, not a valid direcotry");
+    if (argc > 1) {
+        path = argv[1];
+    }
+
+    if ((dir = opendir(path)) == NULL) {
+        printf("Error opening directory");
         exit(1);
     }
 
-    // Variables for reading the directory
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    // Variables for storing file length and names
     char **files = malloc(sizeof(char *));
     int filesLength = 0;
-    
-    // Read the directory, store the file names
-    while ((read = getline(&line, &len, dir)) != -1) {
-        files[filesLength] = malloc(sizeof(char) * (strlen(line) + 1));
-        strcpy(files[filesLength], line);
+
+    while ((dirp = readdir(dir)) != NULL) {
+        files[filesLength] = malloc(strlen(dirp->d_name) + 1);
+        strcpy(files[filesLength], dirp->d_name);
         filesLength++;
         files = realloc(files, sizeof(char *) * (filesLength + 1));
+
     }
 
-    // Print all the files
     for (int i = 0; i < filesLength; i++) {
-        printf("%s", files[i]);
+        for (int j = i + 1; j < filesLength; j++) {
+            if (strcmp(files[i], files[j]) > 0) {
+                char *temp = files[i];
+                files[i] = files[j];
+                files[j] = temp;
+            }
+        }
     }
-    free(line);
-    pclose(dir);
-    exit(0);
+
+    for (int i = 0; i < filesLength; i++) {
+        printf("%s\n", files[i]);
+    }
+    closedir(dir);
+    return 0;
 }
